51378db9032ce6159494d47c1b8730070b89efc6
Parisbre56 <parisbre56@users.noreply.github.com>
2014-05-30 03:48:27 +0300

    Making some final cosmetic changes...

--- test_image.php
+++ test_image.php
@@ -47,10 +47,10 @@ imagestring($im , 1 , 65, 15, $diff_string , $text_color);
 imagestring($im , 1 , 5 , 25, 'days since the last Incident', $text_color);
 imagestring($im , 1 , 5 , 35, "Last Incident: ".$incident_name, $text_color);
 */
-imagettftext($im,10,0,45,10,$text_color,$font_normal,'It has been');
-imagettftext($im,10,0,65,22,$text_color,$font_bold,$diff_string);
-imagettftext($im,10,0,5,34,$text_color,$font_normal,'days since the last Incident');
-imagettftext($im,10,0,5,46,$text_color,$font_normal,"Last Incident: ".$incident_name);
+imagettftext($im,10,0,50,12,$text_color,$font_normal,'It has been');
+imagettftext($im,10,0,75,24,$text_color,$font_bold,$diff_string);
+imagettftext($im,10,0,5,36,$text_color,$font_normal,'days since the last Incident');
+imagettftext($im,10,0,5,48,$text_color,$font_normal,"Last Incident: ".$incident_name);
 
 //Output the image to the browser and destroy it locally.
 imagepng($im);
