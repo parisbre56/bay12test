a75c695271dd7f95696ae433fba01d30683d7eb5
Parisbre56 <parisbre56@users.noreply.github.com>
2014-05-30 03:30:53 +0300

    Testing true typee fonts. Let's see if I can bold this...

--- test_image.php
+++ test_image.php
@@ -6,8 +6,20 @@ $im = @imagecreatetruecolor(150, 50)
 	or die('Cannot Initialize new GD image stream');
 
 //Set background and text color
-$background = imagecolorallocate($im,255,255,255);
+$background = imagecolorallocate($im,0,0,0);
 $text_color = imagecolorallocate($im,50,205,50);
+imagefill($im, 0, 0, $background);
+
+// Set the enviroment variable for GD
+putenv('GDFONTPATH=' . realpath('.'));
+
+// Name the font to be used (note the lack of the .ttf extension)
+$font_normal_name = 'DejaVuSans-Bold';
+$font_bold_name = 'DejaVuSans';
+
+//Get fonts
+$font_normal=imageloadfont($font_normal_name);
+$font_bold=imageloadfont($font_bold_name);
 
 
 
@@ -36,11 +48,16 @@ $diff_string = sprintf('%03d',$diff_days->days);
 
 
 //Write the text to the image.
+/* THESE ARE OLD. They're small and can't be made bold.
 imagestring($im , 1 , 45, 5 , 'It has been' , $text_color);
 imagestring($im , 1 , 65, 15, $diff_string , $text_color);
 imagestring($im , 1 , 5 , 25, 'days since the last Incident', $text_color);
 imagestring($im , 1 , 5 , 35, "Last Incident: ".$incident_name, $text_color);
-
+*/
+imagettftext($im,10,1,45,5,$text_color,$font_normal,'It has been');
+imagettftext($im,10,1,65,15,$text_color,$font_bold,$diff_string);
+imagettftext($im,10,1,5,25,$text_color,$font_normal,'days since the last Incident');
+imagettftext($im,10,1,5,35,$text_color,$font_normal,"Last Incident: ".$incident_name);
 
 //Output the image to the browser and destroy it locally.
 imagepng($im);
