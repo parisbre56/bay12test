f0b2dc0059be46baf7d366a4277d58644f6f4bcf
Parisbre56 <parisbre56@users.noreply.github.com>
2014-05-31 04:03:42 +0300

    Testing autoupdate feature for the server...

--- github.php
+++ github.php
@@ -1,9 +1,9 @@
 <?php
 //Tells the browser to show the output as plain text
 header('Content-type: text/plain');
-echo 'Executing manual refresh...';
+print 'Executing manual refresh...';
 //The git pull command should be put here.
-echo exec('whoami');
-echo exec('ls');
+print exec('whoami');
+print exec('ls');
 ?>
 
