93018fa16967884e9cfef9a33bd3ad16a86c5b4a
Parisbre56 <parisbre56@users.noreply.github.com>
2014-05-31 04:22:29 +0300

    Testing autoupdate feature for the server...

--- github.php
+++ github.php
@@ -1,16 +1,9 @@
 <?php
-	function executeAndPrint($to_exec) {
-		unset($result);
-		exec($to_exec,$result);
-		for($i=0,$size=count($result);$i<$size;++$i) {
-			print $result[$i],'\n';
-		}
-	}
 	//Tells the browser to show the output as plain text
 	header('Content-type: text/plain');
 	print 'Executing manual refresh...\n=================\n';
 	//The git pull command should be put here.
 	print 'Root directory contents before update:\n';
-	executeAndPrint('ls -la');
+	passthru('ls -la');
 ?>
 
