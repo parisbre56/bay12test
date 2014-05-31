c5676360ad0a03de8444a67e98ca425fdfdfe998
Parisbre56 <parisbre56@users.noreply.github.com>
2014-05-31 04:32:19 +0300

    Testing autoupdate feature for the server...

--- github.php
+++ github.php
@@ -1,9 +1,24 @@
 <?php
 	//Tells the browser to show the output as plain text
 	header('Content-type: text/plain');
-	print 'Executing manual refresh...\n=================\n';
+	print 'Executing manual refresh...
+=================
+';
 	//The git pull command should be put here.
-	print 'Root directory contents before update:\n';
+	print '
+Root directory contents before update: ls -la
+';
+	passthru('ls -la');
+	print '
+=================
+
+Attempting to pull data from github: git pull https://github.com/parisbre56/bay12test.git
+';
+	passthru('git pull https://github.com/parisbre56/bay12test.git');
+	print '
+=================
+
+Root directory contents after update: ls -la
+';
 	passthru('ls -la');
 ?>
-
