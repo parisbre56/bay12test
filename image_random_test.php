4a58a7b82f67a858326e190977434a168eeff58e
parisbre56 <parisbre56@users.noreply.github.com>
2014-06-03 19:51:47 +0300

    Fixed image. Random now.

--- image_random_test.php
+++ image_random_test.php
@@ -12,19 +12,19 @@ $index = rand(0,(count($imagearray)/2)-1);
 //Get the selected image
 if ($imagearray[($index*2)+1]=="png") {
   header ('Content-type:image/png');
-  $image=imagecreatefrompng($imagearray[($index*2)+1]);
+  $image=imagecreatefrompng($imagearray[$index*2]);
   imagepng($image);
   imagedestroy($image);
 } 
 else if ($imagearray[($index*2)+1]=="gif") {
   header ('Content-type:image/gif');
-  $image=imagecreatefromgif($imagearray[($index*2)+1]);
+  $image=imagecreatefromgif($imagearray[$index*2]);
   imagegif($image);
   imagedestroy($image);
 }
 else {
   header ('Content-type:text/plain');
-  echo "Unhandled image type " . $imagearray[($index*2)+1] . "
+  echo "Unhandled image type " . $imagearray[($index*2)+1] . " for image " . $imagearray[$index*2] . "
   Edit \"image_random_test.php\" in github to add support";
 }
 
