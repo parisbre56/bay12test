12f943e357ab9ef169beef02b1a0cd0c4620e153
parisbre56 <parisbre56@users.noreply.github.com>
2014-06-03 19:45:57 +0300

    Fixed declarations

--- image_random_test.php
+++ image_random_test.php
@@ -3,8 +3,8 @@
 //Images and their type
 $imagearray=array( 'http://parisbre56-phpexperiment.rhcloud.com/test_image.php','png',
                    'http://parisbre56-phpexperiment.rhcloud.com/test_image_death.php','png',
-                   'http://parisbre56-phpexperiment.rhcloud.com/image_combo_test.php','gif',
-                   'http://parisbre56-phpexperiment.rhcloud.com/image_gif_test.php','png'         );
+                   'http://parisbre56-phpexperiment.rhcloud.com/image_combo_test.php','png',
+                   'http://parisbre56-phpexperiment.rhcloud.com/image_gif_test.php','gif'         );
         
 //Select one at random      
 $index = rand(0,(count($imagearray)/2)-1);
