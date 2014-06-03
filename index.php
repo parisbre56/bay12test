e596bb4833d1c942764de395ca583aba29ad6529
parisbre56 <parisbre56@users.noreply.github.com>
2014-06-03 17:54:20 +0300

    New index should display non-hidden server contents.

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<!doctype html>
+<?php
+header('Content-Type: text/html');
+
+print '<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to OpenShift</title>
+  <title>Welcome to Paris\' OpenShift</title>
 
 <style>
 
 /*!
- * Bootstrap v3.0.0
- *
- * Copyright 2013 Twitter, Inc
- * Licensed under the Apache License v2.0
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Designed and built with all the love in the world @twitter by @mdo and @fat.
- */
+* Bootstrap v3.0.0
+*
+* Copyright 2013 Twitter, Inc
+* Licensed under the Apache License v2.0
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Designed and built with all the love in the world @twitter by @mdo and @fat.
+*/
 
   .logo {
     background-size: cover;
@@ -206,69 +209,42 @@ line-height: 1.4;
 </head>
 <body>
 
-<section class='container'>
+<section class=\'container\'>
           <hgroup>
-            <h1>Welcome to your PHP application on OpenShift</h1>
+            <h1>Welcome to Paris\' PHP application on OpenShift</h1>
+            <h2>Bay12test</h2>
           </hgroup>
-
-
-        <div class="row">
-          <section class='col-xs-12 col-sm-6 col-md-6'>
+          <div class="row">
+          <section class=\'col-xs-12 col-sm-6 col-md-6\'>
             <section>
-              <h2>Deploying code changes</h2>
-                <p>OpenShift uses the <a href="http://git-scm.com/">Git version control system</a> for your source code, and grants you access to it via the Secure Shell (SSH) protocol. In order to upload and download code to your application you need to give us your <a href="https://www.openshift.com/developers/remote-access">public SSH key</a>. You can upload it within the web console or install the <a href="https://www.openshift.com/developers/rhc-client-tools-install">RHC command line tool</a> and run <code>rhc setup</code> to generate and upload your key automatically.</p>
+              <h2>Root folder contents:</h2>
+';
 
-                <h3>Working in your local Git repository</h3>
-                <p>If you created your application from the command line and uploaded your SSH key, rhc will automatically download a copy of that source code repository (Git calls this 'cloning') to your local system.</p>
+$localdir=opendir('.');
+while(($filename=readdir($localdir))!==false) {
+  if($filename[0]!='.') {
+    print "                <p><a href=\"http://parisbre56-phpexperiment.rhcloud.com/\"" . $filename . ">" . $filename . "</a></p>
+";
+  }
+}
 
-                <p>If you created the application from the web console, you'll need to manually clone the repository to your local system. Copy the application's source code Git URL and then run:</p>
-
-<pre>$ git clone &lt;git_url&gt; &lt;directory_to_create&gt;
-
-# Within your project directory
-# Commit your changes and push to OpenShift
-
-$ git commit -a -m 'Some commit message'
-$ git push</pre>
-
-
-                  <ul>
-                    <li><a href="https://www.openshift.com/developers/deploying-and-building-applications">Learn more about deploying and building your application</a></li>
-                    <li><a href="http://openshift.github.io/documentation/oo_cartridge_guide.html#php">Read more details about Repository Layout, Action Hooks and Markers</a></li>
-                  </ul>
-            </section>
+print "            </section>
 
           </section>
-          <section class="col-xs-12 col-sm-6 col-md-6">
-
-                <h2>Managing your application</h2>
-
-                <h3>Web Console</h3>
-                <p>You can use the OpenShift web console to enable additional capabilities via cartridges, add collaborator access authorizations, designate custom domain aliases, and manage domain memberships.</p>
-
-                <h3>Command Line Tools</h3>
-                <p>Installing the <a href="https://www.openshift.com/developers/rhc-client-tools-install">OpenShift RHC client tools</a> allows you complete control of your cloud environment. Read more on how to manage your application from the command line in our <a href="https://www.openshift.com/user-guide">User Guide</a>.
-                </p>
-
-                <h2>Development Resources</h2>
-                  <ul>
-                    <li><a href="https://www.openshift.com/developers">Developer Center</a></li>
-                    <li><a href="https://www.openshift.com/user-guide">User Guide</a></li>
-                    <li><a href="https://www.openshift.com/support">OpenShift Support</a></li>
-                    <li><a href="http://stackoverflow.com/questions/tagged/openshift">Stack Overflow questions for OpenShift</a></li>
-                    <li><a href="http://webchat.freenode.net/?randomnick=1&channels=openshift&uio=d4">IRC channel at #openshift on freenode.net</a></li>
-                    <li><a href="http://git-scm.com/documentation">Git documentation</a></li>
-                  </ul>
-
-
-          </section>
-        </div>
+          
+</div>
 
 
         <footer>
-          <div class="logo"><a href="https://www.openshift.com/"></a></div>
+          <p>Repository and instructions in: <a href=\"https://github.com/parisbre56/bay12test\">https://github.com/parisbre56/bay12test</a></p>
+          <p>Contact Parisbre56 at:</p>
+          <p><a href=\"http://www.bay12forums.com/smf/index.php?action=profile;u=78376\">http://www.bay12forums.com/smf/index.php?action=profile;u=78376</a></p>
+          <p><a href=\"mailto:parisbre56@gmail.com?Subject=Openshift PHP Bay12\">parisbre56@gmail.com</a></p>
+          <div class=\"logo\"><a href=\"https://www.openshift.com/\"></a></div>
         </footer>
 </section>
 
 </body>
-</html>
+</html>";
+
+?>
