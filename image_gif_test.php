196a1568cefd88b0581178422aaacc5fd2fbd929
parisbre56 <parisbre56@users.noreply.github.com>
2014-06-03 19:04:59 +0300

    Testing moving gif image creation

---
+++ image_gif_test.php
@@ -0,0 +1,41 @@
+<?php
+header ('Content-type:image/gif');
+include('GIFEncoder.class.php');
+
+// Open the first source image
+$image = imagecreatefrompng('http://parisbre56-phpexperiment.rhcloud.com/test_image.php');
+
+// Generate GIF from the $image
+
+// We want to put the binary GIF data into an array to be used later,
+//  so we use the output buffer.
+ob_start();
+imagegif($image);
+$frames[]=ob_get_contents();
+
+// Delay in the animation.
+$framed[]=100;
+
+// Clean output buffer.
+ob_end_clean();
+
+// Open the second source image
+$image = imagecreatefrompng('http://parisbre56-phpexperiment.rhcloud.com/test_image_death.php');
+
+// We want to put the binary GIF data into an array to be used later,
+//  so we use the output buffer.
+ob_start();
+imagegif($image);
+$frames[]=ob_get_contents();
+
+// Delay in the animation.
+$framed[]=100;
+
+// Clean output buffer.
+ob_end_clean();
+
+// Generate the animated gif and output to screen.
+$gif = new GIFEncoder($frames,$framed,0,2,0,0,0,'bin');
+echo $gif->GetAnimation();
+
+?>
