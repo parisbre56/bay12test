774d77442104afff493e3ed2f5263f523482a984
Kriellya <tcp@comcast.net>
2014-06-04 12:44:12 -0700

    derp. I can php. I swear

--- test_image.php
+++ test_image.php
@@ -46,7 +46,7 @@ $diff_string = sprintf('%03d',$diff_days->days);
 //Text
 $text_one = 'It has been';
 //$text_two = $diff_string;
-$text_two = "ONGOING, DO NOT PANIC"
+$text_two = 'ONGOING, DO NOT PANIC';
 $text_three = 'days since the last Incident';
 $text_four = "Current: ".$incident_name;
 
