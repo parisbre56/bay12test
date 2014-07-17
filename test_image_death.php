7bfb162413b5b018eb3efa5aedeb02c3f130d1fc
Kriellya <tcp@comcast.net>
2014-07-17 11:59:29 -0700

    7 is going to be too short

--- test_image_death.php
+++ test_image_death.php
@@ -44,7 +44,7 @@ $text_two = $diff_string;
 $text_three = 'days since the last Death';
 $text_four = "Last: ".$death_name;
 
-$width = strlen($text_four) * 8;
+$width = strlen($text_four) * 7;
 if($width > $image_x)
 	$image_x = $width;
 
