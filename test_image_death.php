267f85599636695875ef8b6cb83689e98ab825a5
Kriellya <tcp@comcast.net>
2015-07-24 16:58:18 -0700

    semicolons!!!

--- test_image_death.php
+++ test_image_death.php
@@ -38,12 +38,13 @@ $current_date = new DateTime('now');
 $diff_days =  $death_date->diff($current_date);
 
 // 'Progress bar' calculations
-$deathSS = 5
-$crewTot = 15
-//$ttl = $diff_days->days * ($crewTot - $deathSS) / $deathSS
-//$diff_days = $ttl
+$deathSS = 5;
+$crewTot = 15;
+$ttl = $diff_days->days * ($crewTot - $deathSS) / $deathSS;
+$diff_string = sprintf('%03d',$ttl);
+//$diff_days = $ttl;
 
-$diff_string = sprintf('%03d',$diff_days->days);
+//$diff_string = sprintf('%03d',$diff_days->days);
 
 //Text
 $text_one = 'It has been';
