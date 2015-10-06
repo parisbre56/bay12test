22368de94832686955197c883bede6f24dcd9d43
parisbre56 <parisbre56@users.noreply.github.com>
2015-10-06 18:38:50 +0300

    testing new script

--- get_character.php
+++ get_character.php
@@ -1,5 +1,20 @@
 <?php
 header('Content-Type: text/html');
 
-echo file_get_contents('http://einsteinianroulette.wikia.com/wiki/'+$_GET["name"]);
+$c = curl_init('http://einsteinianroulette.wikia.com/wiki/'.$_GET["name"]);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($c, CURLOPT_MAXREDIRS, 5);
+
+$html = curl_exec($c);
+
+if (curl_error($c))
+    die(curl_error($c));
+
+// Get the status code
+$status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+
+curl_close($c);
+
+echo $html;
 >
