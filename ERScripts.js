aca948da1945da4dbfb3c50328412faf25a2ab88
parisbre56 <parisbre56@users.noreply.github.com>
2015-10-06 16:58:58 +0300

    testing url requests

--- ERScripts.js
+++ ERScripts.js
@@ -15,6 +15,34 @@ var oldonload = window.onload;
 	}
 }
 
+/**
+ * Get HTML asynchronously
+ * @param  {String}   url      The URL to get HTML from
+ * @param  {Function} callback A callback funtion. Pass in "response" variable to use returned HTML.
+ */
+var getHTML = function ( url, callback ) {
+
+	// Feature detection
+	if ( !window.XMLHttpRequest ) return;
+	
+	// Create new request
+	var xhr = new XMLHttpRequest();
+	
+	// Setup callback
+	xhr.onload = function() {
+		if ( callback && typeof( callback ) === 'function' ) {
+			callback( this.responseXML );
+		}
+	}
+	
+	// Get the HTML
+	xhr.open( 'GET', url );
+	xhr.responseType = 'document';
+	xhr.send();
+
+};
+
+
 //Check if the element contains class cls
 function hasClass(element, cls) {
 return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
